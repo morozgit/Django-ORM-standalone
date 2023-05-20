4dbf6ceee4402439970a8be6837dc2e02f3f5e4f
morozgit <morozec76@gmail.com>
2023-05-20 23:02:38 +0300

    add get passcode

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 class Passcard(models.Model):
@@ -28,3 +30,14 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_duration(self, visit):
+        delta = timezone.localtime(visit.leaved_at) - timezone.localtime(visit.entered_at)
+        return delta.seconds
+    
+    def format_duration(self, duration):
+        return datetime.timedelta(seconds=duration)
+    
+    def is_visit_long(self, visit, minutes=60):
+        return self.get_duration(visit) > minutes
+    
--- main.py
+++ main.py
@@ -6,6 +6,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Visit
+from django.utils import timezone
+import datetime
+
+
 
 if __name__ == '__main__':
     # Программируем здесь
@@ -17,4 +22,32 @@ if __name__ == '__main__':
     #     if passcard.is_active:
     #         active_passcards.append(passcard)
     print('Активных пропусков', len(passcards.filter(is_active=True)))
+    
+    visits = Visit.objects.all()
+    # print(visits)
+    print('Кто не вышел', visits.filter(leaved_at=None))
+    print('Текущее время', timezone.localtime())
+    # for visit in visits:
+        # if visit.leaved_at is not None:
+            # delta = timezone.localtime() - timezone.localtime(visit.entered_at)
+            # print(visit)
+            # print('Зашёл в хранилище, время по Москве:\n{0}'.format(timezone.localtime(visit.entered_at)), '\nНаходится в хранилище:\n{0}'.format(visit.get_duration(visit)))
+    # print(len(visits), len(visits.filter(leaved_at=None)))
+    print('Кто в хранилище:')
+    for visit in visits:
+        if visit.leaved_at is None:
+            print(visit.passcard.owner_name,end='\n')
+
+    
+    p = passcards.get(passcode=passcards[0].passcode)
+    one_person_visit = visits.filter(passcard=p)
+    print(one_person_visit)
+    # for one_visit in one_person_visit:
+    #     if one_visit.is_visit_long(one_visit,10):
+    #         print('Визиты дольше 10 мин', one_visit)
+    #     elif one_visit.is_visit_long(one_visit,10):
+    #         print('Визиты дольше 1000 мин', one_visit)
+    #     elif(one_visit.leaved_at is None):
+    #         print('Еще в хранилище')
+    
     
